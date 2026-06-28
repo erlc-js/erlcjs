d83614e6e44443290c543d252793824c543b6c7c
Noinkin <87577662+Noinkin@users.noreply.github.com>
2026-06-28 13:57:27 +0100

    feat: add exports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -13,12 +13,20 @@ export * from './managers/commandmanager.js';
 export * from './managers/playermanager.js';
 export * from './managers/servermanager.js';
 export * from './managers/vehiclemanager.js';
+export * from './managers/commandlogmanager.js';
+export * from './managers/emergencycallmanager.js';
+export * from './managers/killlogmanager.js';
+export * from './managers/modcallmanager.js';
 
 // Structures
 export * from './structures/base.js';
 export * from './structures/player.js';
 export * from './structures/server.js';
 export * from './structures/vehicle.js';
+export * from './structures/commandlog.js';
+export * from './structures/emergencycall.js';
+export * from './structures/killlog.js';
+export * from './structures/modcall.js';
 
 // Types
 export * from './types/index.js';
